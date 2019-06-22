f6b8f241d483d11f13c21a1461a80971e0c850a3
@nrik86 <enrique.jim.1986@gmail.com>
2019-06-22 22:35:32 +0200

    Add profile Fragment and ViewModel

---
+++ app/src/main/java/com/enriquejimenez/minitwitter/mvvm/profile/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.enriquejimenez.minitwitter.mvvm.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
